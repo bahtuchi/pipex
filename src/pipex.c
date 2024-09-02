e9d2b34111430afc31312c8746ee379156774fc3
Omed Mehdizada <omed@konnektiv.de>
2024-09-02 18:26:17 +0200

    almost done

--- src/pipex.c
+++ src/pipex.c
@@ -6,10 +6,9 @@
 /*   By: omed <omed@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/31 08:29:30 by omed              #+#    #+#             */
-/*   Updated: 2024/08/31 23:46:13 by omed             ###   ########.fr       */
+/*   Updated: 2024/09/02 16:56:52 by omed             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 /*
 functions to be used:
@@ -41,12 +40,58 @@ typedef struct s_pipeline
 	int		tube[2];
 	int		pid1;
 	int		pid2;
-	char	*command;
+	char	**command;
 	char	*command_path;
 	char	**splitted_path;
 	char	*env_path;
 }	t_pipeline;
 
+void free_all(t_pipeline pipex)
+{
+	int	i;
+
+	i = 0;
+	while(pipex.command[i])
+	{
+		free(pipex.command[i]);
+		i++;
+	}
+	free(pipex.command[i]);
+	i = 0;
+	while(pipex.splitted_path[i])
+	{
+		free(pipex.splitted_path[i]);
+		i++;
+	}
+	free(pipex.splitted_path[i]);
+	exit(EXIT_FAILURE);	
+}
+
+void child(t_pipeline pipex, char **argv,char **envp)
+{
+	dup2(pipex.tube[1], 1);
+	close(pipex.tube[0]);
+	dup2(pipex.infile, 0);
+	pipex.command = ft_split(argv[2], ' ');
+	pipex.command_path = command_to_path(pipex.splitted_path, pipex.command);
+	if (pipex.command_path == NULL)
+		free_all(pipex);
+	else
+		execve(pipex.command_path, pipex.command, envp);
+}
+
+void parent(t_pipeline pipex, char **argv,char **envp)
+{
+	dup2(pipex.tube[0], 0);
+	close(pipex.tube[1]);
+	dup2(pipex.outfile, 1);
+	pipex.command = ft_split(argv[2], ' ');
+	pipex.command_path = command_to_path(pipex.splitted_path, pipex.command);
+	if (pipex.command_path == NULL)
+		free_all(pipex);
+	else
+		execve(pipex.command_path, pipex.command, envp);
+}
 
 char *path_extract(char **envp)
 {
@@ -54,147 +99,86 @@ char *path_extract(char **envp)
 
 	i = 0;
 	while (ft_strncmp("PATH", envp[i], 4) != 0)
-	{
 		i++;
-	}
 	return (envp[i] + 5);
 }
 
-char	*command_to_path(char **envp, char *cmd)
+char	*command_to_path(char **splitted_path, char *cmd)
 {
-		t_pipeline pipex;
+	t_pipeline pipex;
 
 	//printf("%s", path_extract(envp));
 
 	int i = 0;
 	while(pipex.splitted_path[i])
 	{
-		if((ft_strncmp("/usr/bin", pipex.splitted_path[i], 8) == 0))
-		{
 			pipex.command_path = ft_strjoin(pipex.splitted_path[i], "/");
 			pipex.command = ft_strjoin(pipex.command_path, cmd);
-			free(pipex.splitted_path);
 			free(pipex.command_path);
 			if(access(pipex.command, X_OK) == 0)
 				return(pipex.command);
 			free(pipex.command);
 			i++;
-		}
-	i++;
 	}
+	return (NULL);
 }
 
 char *full_path(char *cmd, char **envp)
 {
 	t_pipeline pipex;
-	
-	pipex.splitted_path = ft_split(path_extract(envp), ':');
-	
 	int	i;
-
+	
+	
+	pipex.command_path = command_to_path(pipex.splitted_path, cmd);
 	i = 0;
-	while(pipex.splitted_path)
-}
-
-/*
-void ft_putstr(char *str)
-{
-	int	i;
-
-	i = 0;
-	while(str[i])
+	while(pipex.splitted_path[i])
 	{
-		write(1, str + i, 1);
-		i++;
+		free(pipex.splitted_path[i]);
+		i++;	
 	}
-}
-
-//Maybe not needed
-void	check_files(int argc, char **argv)
-{
-
-}
-
-void	free_everything(t_pipeline pipeline)
-{
-
+	free(pipex.splitted_path);
+	return (pipex.command_path);
 }
 
 
-char	*get_path(char **splitted_path, char *command)
+void file_checker(int argc, char **argv)
 {
-	t_pipeline pipex;
-
-
-
-}
-
-char	*find_path(char **envp)
-{
-	t_pipeline pipex;
-
-}
-
-
-int main(int argc, char **argv, char **envp)
-{
-	t_pipeline pipex;
-
-
-    if (argc != 5)
+	
+	if (argc != 5)
     {
-		ft_putstr("Function must have 4 Arguments!");
+		ft_putstr_fd("Function must have 4 Arguments!", 1);
 		exit (EXIT_FAILURE);   
 	}
-	if (access(argv[1], R_OK) == -1)
-	{
-		ft_putstr("wrong Input-File");
+	if (access(argv[1], F_OK) == -1 || access(argv[4], F_OK) == -1 )
+    {
+		ft_putstr_fd("Something wrong with your Files", 1);
 		exit (EXIT_FAILURE);   
 	}
-	if (access(argv[5], W_OK || R_OK) == -1)
-	{
-		ft_putstr("wrong Output-File");
-		exit (EXIT_FAILURE); 
+	else if (access(argv[1], R_OK) == -1 || access(argv[4], W_OK | R_OK) == -1 )
+    {
+		ft_putstr_fd("Check your File permissions!", 1);
+		exit (EXIT_FAILURE);   
 	}
-	if (pipe(pipex.tube) == -1)
-	{
-		return 1;
-	}
-
-	pipex.pid1 = fork();
-	if (pipex.pid1 < 0)
-	{
-		return 2;
-	}
-//child
-	if (pipex.pid1 == 0)
-	{
-		dup2(pipex.tube[1], STDOUT_FILENO);
-		close(pipex.tube[0]);
-		close(pipex.tube[1]);
-		execve("", NULL);
-	}
-//other child
-	pipex.pid2 = fork();
-	if (pipex.pid2 < 0)
-	{
-		dup2(pipex.tube[0], STDIN_FILENO);
-		close(pipex.tube[0]);
-		close(pipex.tube[1]);
-		return 3;
-	}
-
-
-}*/
+}
 
 int main (int argc, char **argv, char **envp)
 {
 	t_pipeline pipex;
 
-	/*if (argc != 5)
-    {
-		ft_putstr_fd("Function must have 4 Arguments!", 1);
-		exit (EXIT_FAILURE);   
-	}*/
-	//printf("%s", path_extract(envp));
+	file_checker(argc, argv);
+	pipex.infile = open(argv[1], O_RDONLY);
+	pipex.outfile = open(argv[4], O_RDWR);
+	pipex.env_path = path_extract(envp);
+	pipex.splitted_path = ft_split(path_extract(envp), ':');
+	pipe(pipex.tube);
+	pipex.pid1 = fork();
+	if(pipex.pid1 == 0)
+		child(pipex, argv, envp);
+	else	
+		parent(pipex, argv, envp);
+	close(pipex.tube[1]);
+	close(pipex.tube[0]);
+	waitpid(pipex.pid1, NULL, 0);
+	free_all(pipex);
+	return (0);
 }
